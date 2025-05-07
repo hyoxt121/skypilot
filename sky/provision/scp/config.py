e4009e67c717e5e206cad5cf5368341a4b329739
Kihyo Moon <36657450+hyoxt121@users.noreply.github.com>
2025-05-07 10:09:42 +0900

    new provisioner

--- sky/provision/scp/config.py
+++ sky/provision/scp/config.py
@@ -1,7 +1,6 @@
 """SCP configuration bootstrapping."""
 
-from requests import auth
-
+import subprocess
 from sky.provision.scp import utils
 from sky.provision import common
 
@@ -16,32 +15,32 @@ def bootstrap_instances(
 
     node_cfg = config.node_config
     zone_id = _get_zone_id(region)
-    node_cfg['zone_id'] = zone_id
+    node_cfg["zone_id"] = zone_id
 
     authentication_cfg = config.authentication_config
-    ssh_user = authentication_cfg['ssh_user']
-    ssh_public_key = node_cfg['AuthorizedKey']
+    ssh_user = authentication_cfg["ssh_user"]
+    ssh_public_key = node_cfg["AuthorizedKey"]
 
     docker_cfg = config.docker_config
-    docker_cfg['imageId'] = node_cfg['imageId']
-    docker_cfg['serviceZoneId'] = zone_id
-    docker_cfg['serverType'] = node_cfg['InstanceType']
-    docker_cfg['contractId'] = "None"
+    docker_cfg["imageId"] = node_cfg["imageId"]
+    docker_cfg["serviceZoneId"] = zone_id
+    docker_cfg["serverType"] = node_cfg["InstanceType"]
+    docker_cfg["contractId"] = "None"
     initial_script = _get_vm_init_script(ssh_public_key)
-    docker_cfg['initialScript'] = initial_script
+    docker_cfg["initialScript"] = initial_script
 
     miscellaneous = {
-        'deletionProtectionEnabled': False,
-        'dnsEnabled': True,
-        'osAdmin': {
-            'osUserId': ssh_user,
-            'osUserPassword': 'default!@&$351!'
+        "deletionProtectionEnabled": False,
+        "dnsEnabled": True,
+        "osAdmin": {
+            "osUserId": ssh_user,
+            "osUserPassword": "default!@&$351!"
         },
-        'blockStorage': {
-            'blockStorageName': 'skystorage',
-            'diskSize': node_cfg['diskSize'],
-            'encryptEnabled': False,
-            'productId': 'PRODUCT-sRlJ34iBr9hOxN9J5PrQxo'
+        "blockStorage": {
+            "blockStorageName": "skystorage",
+            "diskSize": node_cfg["diskSize"],
+            "encryptEnabled": False,
+            "productId": "PRODUCT-sRlJ34iBr9hOxN9J5PrQxo"
         },
         "nic": {
             "natEnabled": True
@@ -56,18 +55,21 @@ def bootstrap_instances(
 def _get_zone_id(region_name: str):
     zone_contents = client.get_zones()
     zone_dict = {
-        item['serviceZoneName']: item['serviceZoneId'] for item in zone_contents
+        item["serviceZoneName"]: item["serviceZoneId"] for item in zone_contents
     }
     return zone_dict[region_name]
 
 
 def _get_vm_init_script(ssh_public_key: str):
-    import subprocess
     init_script_content = _get_default_config_cmd() + _get_ssh_key_gen_cmd(
         ssh_public_key)
     init_script_content_string = f'"{init_script_content}"'
-    command = f'echo {init_script_content_string} | base64'
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    command = f"echo {init_script_content_string} | base64"
+    result = subprocess.run(command,
+                            shell=True,
+                            capture_output=True,
+                            text=True,
+                            check=True)
     init_script_content_base64 = result.stdout
     return {
         "encodingType": "base64",
