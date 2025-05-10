f8a5b4cc886c679c1583e1ff3164d13a5915e459
Kihyo Moon <36657450+hyoxt121@users.noreply.github.com>
2025-05-10 16:12:09 +0900

    Update scp_utils.py

--- sky/clouds/utils/scp_utils.py
+++ sky/clouds/utils/scp_utils.py
@@ -59,8 +59,8 @@ class Metadata:
             metadata = json.load(f)
         return metadata.get(instance_id)
 
-    def __setitem__(self, instance_id: str,
-                    value: Optional[Dict[str, Any]]) -> None:
+    def __setitem__(self, instance_id: str, value: Optional[Dict[str,
+                                                                 Any]]) -> None:
         # Read from metadata file
         if os.path.exists(self.path):
             with open(self.path, 'r', encoding='utf-8') as f:
@@ -165,8 +165,7 @@ class SCPClient:
         with open(self.credentials, 'r', encoding='utf-8') as f:
             lines = [line.strip() for line in f.readlines() if ' = ' in line]
             self._credentials = {
-                line.split(' = ')[0]: line.split(' = ')[1]
-                for line in lines
+                line.split(' = ')[0]: line.split(' = ')[1] for line in lines
             }
         self.access_key = self._credentials['access_key']
         self.secret_key = self._credentials['secret_key']
@@ -311,16 +310,11 @@ class SCPClient:
                 'serviceType': 'TCP',
                 'serviceValue': '443'
             }],
-            'ruleDirection':
-            'OUT',
-            'ruleAction':
-            'ALLOW',
-            'isRuleEnabled':
-            True,
-            'ruleLocationType':
-            'FIRST',
-            'ruleDescription':
-            'description'
+            'ruleDirection': 'OUT',
+            'ruleAction': 'ALLOW',
+            'isRuleEnabled': True,
+            'ruleLocationType': 'FIRST',
+            'ruleDescription': 'description'
         }
         return self._post(url, request_body)
 
@@ -366,8 +360,8 @@ class SCPClient:
             int(
                 round(
                     datetime.datetime.timestamp(datetime.datetime.now() -
-                                                datetime.timedelta(minutes=1))
-                    * 1000)))
+                                                datetime.timedelta(minutes=1)) *
+                    1000)))
         self.headers['X-Cmp-Timestamp'] = self.timestamp
 
     def set_signature(self, method: str, url: str) -> None:
@@ -451,8 +445,8 @@ class SCPClient:
         url = f'{API_ENDPOINT}/firewall/v2/firewalls/{firewall_id}/rules'
         return self._get(url)
 
-    def _check_existing_firewall_rule(self, firewall_id, internal_ip,
-                                      direction, ports):
+    def _check_existing_firewall_rule(self, firewall_id, internal_ip, direction,
+                                      ports):
         response = self.get_firewall_rules(firewall_id)
         rules = []
         for rule in response:
@@ -469,8 +463,7 @@ class SCPClient:
                 return False
         return True
 
-    def add_new_firewall_rule(self, firewall_id, internal_ip, direction,
-                              ports):
+    def add_new_firewall_rule(self, firewall_id, internal_ip, direction, ports):
         if self._check_existing_firewall_rule(firewall_id, internal_ip,
                                               direction, ports):
             url = f'{API_ENDPOINT}/firewall/v2/firewalls/{firewall_id}/rules'
